fec9fade0823570ebb93a470199637a6582dc174
MLT05 <marianatavares18@ua.pt>
2026-05-14 18:42:05 +0100

    contact-frase

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -3,19 +3,18 @@ function ContactSection() {
         <section className="contact-section scroll-reveal scroll-reveal-delay-1">
             <div className="container contact-inner">
                 <h2 className="contact-title scroll-reveal scroll-reveal-delay-1">
-                    Vamos entrar em contacto
+                    Vamos entrar em contacto!
                 </h2>
 
                 <p className="contact-text scroll-reveal scroll-reveal-delay-2">
-                    Adorava continuar a construir o meu portfólio com novos projetos
-                    digitais, websites e experiências focadas no utilizador.
+                    Gostava de continuar a fazer crescer o meu portefólio com novos projetos.
                 </p>
 
                 <a
                     href="mailto:marianalopestavares18@gmail.pt"
                     className="contact-button scroll-reveal scroll-reveal-delay-3"
                 >
-                    Entrar em contacto comigo
+                    Entrar em contacto
                 </a>
             </div>
         </section>
