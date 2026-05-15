9e843c181e56f44c316a1fec8d96a9f2f7802956
MLT05 <marianatavares18@ua.pt>
2026-05-15 09:43:24 +0100

    mais experiencia

--- src/pages/work_pages/WorkCards.jsx
+++ src/pages/work_pages/WorkCards.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import projectsData from "../data/projectsData"
+import projectsData from "../../data/projectsData.js"
 
 function WorkCards() {
     return (
